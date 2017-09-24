567f5f4199054d31b7f440a73a84cc1ce9414764
smishra38@sapient.com <smishra38@sapient.com>
2017-09-24 22:00:59 +0530

    Changes for Heroku

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const config = require('./config');
-
+const path = require('path');
 // connect to the database and load models
 require('./server/models').connect(config.dbUri, {
   useMongoClient: true,
