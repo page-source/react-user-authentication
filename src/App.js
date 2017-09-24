4cb2926075fb9c6c2329391de4be459aa5ab29df
smishra38@sapient.com <smishra38@sapient.com>
2017-09-25 01:38:47 +0530

    Homepage changes when logged in

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import {amber, red, pink} from 'material-ui/colors'
 import createPalette from 'material-ui/styles/createPalette'
 import {
-  BrowserRouter as Router, Route
+  BrowserRouter as Router, Route, Redirect
 } from 'react-router-dom'
 import Base from './components/Base';
 import HomePage from './components/HomePage';
@@ -12,7 +12,7 @@ import LoginPage from './containers/LoginPage';
 import SignUpPage from './containers/SignUpPage';
 import DashboardPage from './containers/DashboardPage';
 import Logout from './containers/Logout';
-
+import Auth from './modules/Auth';
 const muiTheme = createMuiTheme({
     palette: createPalette({
       primary: pink,
@@ -27,9 +27,13 @@ const App = () =>(
 	    <Router>
 	  		<div>
 	  			<Base color="accent"/>
-	  			<Route exact path="/" render={props => 
-	            	<HomePage {...props} />
-	        	}/>
+	  			<Route exact path="/" render={props => (
+	            	Auth.isUserAuthenticated() ? (
+	            		<Redirect to="/dashboard"/>
+	            	) : (
+					    <HomePage/>
+					)
+	        	)}/>
 		        <Route exact path="/dashboard" render={props => 
 		            <DashboardPage />
 		        }/>
